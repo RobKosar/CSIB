6885c043048176dffc7f5e86c8c26a060f3e527e
roberto2kk <robertmichaelkosar@gmail.com>
2026-07-01 10:17:19 -0400

    Notes

--- Notes/structures.cpp
+++ Notes/structures.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 using std::cin, std::cout;
 
+// Kind of like a class, holds information and can be used to make variables that hold lots of info
 struct Date {
     int day;
     int month;
     int year;
 };
 
-int main(){
+int main() {
+    Date today; // Referencing the structure to make new variable
 
+    today.day = 1; // Assignment, just like python
+    today.month = 7;
+    today.year = 2026; 
+    // Today now represents 07/1/2026
+
+    // Let's make one for my birthday using a different assignment method
+    Date birthday = {21, 7, 2006};
+    cout << birthday.month << '/' << birthday.day << '/' << birthday.year;
 }
